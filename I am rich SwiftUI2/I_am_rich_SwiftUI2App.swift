c8416ce7b3f495d2959ca3e7dab05ad19520dcee
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-10-08 21:01:39 -0700

    Initial Commit

---
+++ I am rich SwiftUI2/I_am_rich_SwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_rich_SwiftUI2App.swift
+//  I am rich SwiftUI2
+//
+//  Created by Nastya Klyashtorna on 2024-10-08.
+//
+
+import SwiftUI
+
+@main
+struct I_am_rich_SwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
